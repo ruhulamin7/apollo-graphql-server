ff0d11f345013964b84fada052d6cc09799ef9f2
ruhulaminbd93 <ruhulaminbd93@gmail.com>
2023-01-28 12:23:59 +0600

    add user mutation

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -5,6 +5,28 @@ const resolvers = {
     users: (root, args) => {
       return users;
     },
+    user: (root, { id }) => {
+      const user = users.find((user) => user.id == id);
+      return user;
+    },
+  },
+
+  Mutation: {
+    addUser: (
+      root,
+      { input: { firstName, lastName, phone, email, isMarred } }
+    ) => {
+      const user = {
+        firstName,
+        lastName,
+        phone,
+        email,
+        isMarred,
+        posts: [],
+      };
+      users.push(user);
+      return user;
+    },
   },
 };
 
--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -1,22 +1,39 @@
 const typeDefs = `#graphql 
     type User {
-        id: ID!
-        firstName : String!
-        lastName:String!
-        # gender : GenderEnum!
-        phone:String!
-        email:String!
-        isMarried:Boolean!
+        id: ID
+        firstName: String!
+        lastName: String
+        gender: GenderEnum
+        phone: String
+        email: String
+        isMarried: Boolean
         #posts:[post!]
     }
+  
 
-    enum GangerEnum {
+    enum GenderEnum {
         Female
         Male
     }
 
+    input UserInput {
+            id: ID
+            firstName : String!
+            lastName:String
+            gender : GenderEnum
+            phone:String
+            email:String
+            isMarried:Boolean
+            #posts:[post!]
+    }
+
     type Query {
         users: [User!]!
+        user(id:ID!):User!
+    }
+
+    type Mutation {
+        addUser(input:UserInput):User
     }
 
   
